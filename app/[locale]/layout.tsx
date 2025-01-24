b816aa6ea1890028a39bcfe2a9380631b9f0f733
Costa <afonsovelosof@gmail.com>
2025-01-24 16:10:37 +0000

    Add HomePage, layout, and page components for application structure and data handling

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,57 @@
+import '../globals.css';
+import { Poppins } from 'next/font/google';
+import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from '../head';
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800'],
+  variable: '--font-poppins',
+  display: 'swap', // Ensures the font swaps gracefully
+});
+
+export default async function RootLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: {locale: string};
+}) {
+  const { locale } = await params;
+
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+
+  return (
+    <html lang={locale}>
+      <Head />
+      <body className={`${poppins.className} bg-gray-100 dark:bg-grey-900 text-black dark:text-white min-h-screen`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="min-h-screen">
+          <NextIntlClientProvider messages={messages}>
+            {children}
+            </NextIntlClientProvider>
+
+            <SpeedInsights />
+            <Analytics />
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/[locale]/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect, useState } from "react";
+import { FaNodeJs } from "react-icons/fa";
+import HomePage from "./HomePage";
+import data from "@/data.json";
+// import { ref, get } from "firebase/database";
+// import { database } from "@/firebase";
+
+async function getData() {
+  // return await (await get(ref(database))).val();
+  /*
+   const DB_URL = process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL + '/.json'
+   const res = await fetch(DB_URL, { cache: 'no-store' })
+   const data = res.json()
+   return data */
+
+  return data;
+}
+
+export default function Page() {
+  const [data, setData] = useState<typeof import("@/data.json") | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await getData();
+      setData(result);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <HomePage data={data} />
+      ) : (
+        <div className="h-screen w-screen flex flex-col items-center justify-center gap-5 text-violet-600 fixed z-30 bg-gray-100 dark:bg-grey-900">
+          <FaNodeJs size={100} className="animate-pulse" />
+          <p className="animate-pulse text-xl">Loading...</p>
+        </div>
+      )}
+    </>
+  );
+}
